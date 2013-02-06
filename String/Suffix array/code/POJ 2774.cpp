5f20fa39304a9682b396647efc6dd442d4ed968f
strongoier <xy_xuyi@foxmail.com>
2013-02-06 19:19:45 +0800

    Update

---
+++ String/Suffix array/code/POJ 2774.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstring>
+
+const int MAXN = 222222;
+
+char s[MAXN];
+int c[MAXN], ht[MAXN], sa[MAXN], nsa[MAXN], rk[MAXN], nrk[MAXN][2];
+
+int main() {
+    gets(s);
+    int m = strlen(s);
+    s[m] = '#';
+    gets(s + m + 1);
+    int n = strlen(s);
+    for (int i = 0; i < n; ++i)
+        ++c[s[i]];
+    for (int i = 0; i < 333; ++i)
+        c[i + 1] += c[i];
+    for (int i = n - 1; i >= 0; --i)
+        sa[--c[s[i]]] = i;
+    rk[sa[0]] = 1;
+    for (int i = 1; i < n; ++i)
+        rk[sa[i]] = rk[sa[i - 1]] + (s[sa[i]] != s[sa[i - 1]]);
+    for (int k = 1; k < n; k <<= 1) {
+        memset(c, 0, sizeof c);
+        for (int i = 0; i < n; ++i) {
+            nrk[i][0] = rk[i];
+            ++c[nrk[i][1] = i + k < n ? rk[i + k] : 0];
+        }
+        for (int i = 0; i < n; ++i)
+            c[i + 1] += c[i];
+        for (int i = n - 1; i >= 0; --i)
+            nsa[--c[nrk[i][1]]] = i;
+        memset(c, 0, sizeof c);
+        for (int i = 0; i < n; ++i)
+            ++c[nrk[i][0]];
+        for (int i = 0; i < n; ++i)
+            c[i + 1] += c[i];
+        for (int i = n - 1; i >= 0; --i)
+            sa[--c[nrk[nsa[i]][0]]] = nsa[i];
+        rk[sa[0]] = 1;
+        for (int i = 1; i < n; ++i)
+            rk[sa[i]] = rk[sa[i - 1]] + (nrk[sa[i]][0] != nrk[sa[i - 1]][0] || nrk[sa[i]][1] != nrk[sa[i - 1]][1]);
+    }
+    for (int i = 0, k = 0; i < n; ++i)
+        if (--rk[i]) {
+            int j = sa[rk[i] - 1];
+            while (i + k < n && j + k < n && s[i + k] == s[j + k])
+                ++k;
+            ht[rk[i]] = k ? k-- : k;
+        }
+    int ans = 0;
+    for (int i = 1; i < n; ++i)
+        if ((sa[i] < m && sa[i - 1] > m || sa[i] > m && sa[i - 1] < m) && ht[i] > ans)
+            ans = ht[i];
+    printf("%d\n", ans);
+    return 0;
+}
